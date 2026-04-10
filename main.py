c9cc9b5608ceb7ff6dfea79a63f192bc90e4793a
YumnaMarawi <mrawiyumna@gmail.com>
2026-04-10 03:10:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+name=input("enter your name:\n")
+age= int(input("Enter your age:\n "))
+print("welcome"+"  "+name )
+if (age>=18):
+    print("you are eligible to vote congrate!")
+else:
+    print(" we are sorry ! , but you are not eligible to vote")
